f6a0e32fd6802200ec1ca0d74c30cc2717d56f45
ghobadh <ghobadh@gmail.com>
2024-10-02 12:55:55 -0400

    This is an example of  how we set up spring default profile     By adding "default" to @Profile, we don't need active profile in application.properties anymore.      without profile.active nor "default" in @Profile, spring will fail to choose which service profile has to select for the controller     NOTE, Spring.profile.active has higher priority than "default" in the @Profile annotation

--- src/main/java/ca/gforcesoftware/gfsdi/services/I18nSpanishGreetingService.java
+++ src/main/java/ca/gforcesoftware/gfsdi/services/I18nSpanishGreetingService.java
@@ -7,7 +7,11 @@ import org.springframework.stereotype.Service;
  * @author gavinhashemi on 2024-10-02
  * We defined the qualifiter not with @Qualifier annotation but with defining in front of @Service annotation
  */
-@Profile("ES")
+
+    //By adding "default" to @Profile, we don't need active profile in application.properties anymore.
+    // without profile.active nor "default" in @Profile, spring will fail to choose which service profile has to select for the controller
+    //NOTE, Spring.profile.active has higher priority than "default" in the @Profile annotation
+@Profile({"ES", "default"})
 @Service("i18nService")
 public class I18nSpanishGreetingService implements GreetingService {
     @Override
